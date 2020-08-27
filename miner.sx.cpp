650084dad458c1c604e22e4a53aca832e5e52dbb
Denis <carriere.denis@gmail.com>
2020-08-27 17:41:46 -0400

    add steps

--- miner.sx.cpp
+++ miner.sx.cpp
@@ -8,14 +8,71 @@ void minerSx::mine( const name executor )
 {
     require_auth( executor );
 
-    const asset quantity = asset{500000, symbol{"EOS", 4}};
+    const asset quantity = asset{100'0000, symbol{"EOS", 4}};
     const name contract = "eosio.token"_n;
-
-    borrow( contract, quantity );
+    borrow( contract, quantity, executor.to_string() );
 }
+
+// Defibox pairs
+// 9 = EOS/USN
+// 12 = EOS/USDT
+// 35 = USN/USDT
+// 194 = EOS/BOX
 
 [[eosio::on_notify("flash.sx::callback")]]
 void minerSx::on_callback( const name to, const name contract, asset quantity, const string memo, const name recipient )
 {
-    repay( contract, quantity );
+    minerSx::one_action one( get_self(), { get_self(), "active"_n });
+    minerSx::two_action two( get_self(), { get_self(), "active"_n });
+    minerSx::three_action three( get_self(), { get_self(), "active"_n });
+    minerSx::four_action four( get_self(), { get_self(), "active"_n });
+
+    transfer( get_self(), "swap.defi"_n, contract, quantity / 2, "swap,0,9" );
+    transfer( get_self(), "swap.defi"_n, contract, quantity / 2, "swap,0,9" );
+    one.send();
+    two.send();
+    three.send( contract, quantity );
+    four.send( memo );
+}
+
+[[eosio::action]]
+void minerSx::one()
+{
+    require_auth( get_self() );
+
+    const name contract = "danchortoken"_n;
+    const asset balance = token::get_balance( contract, get_self(), symbol_code{"USN"} );
+    check( balance.amount > 0, "error in step 1");
+    transfer( get_self(), "swap.defi"_n, contract, balance, "swap,0,9" );
+}
+
+[[eosio::action]]
+void minerSx::two()
+{
+    require_auth( get_self() );
+
+    const name contract = "token.defi"_n;
+    const asset balance = token::get_balance( contract, get_self(), symbol_code{"BOX"} );
+    check( balance.amount >= 1, "no BOX tokens");
+    transfer( get_self(), "swap.defi"_n, contract, balance, "swap,0,194" );
+}
+
+void minerSx::three( const name contract, const asset quantity )
+{
+    require_auth( get_self() );
+
+    const asset balance = token::get_balance( contract, get_self(), quantity.symbol.code() );
+    const asset remaining = balance - quantity;
+    check( remaining.amount >= 0, "cannot repay flashloan by " + remaining.to_string() );
+    transfer( "miner.sx"_n, "flash.sx"_n, contract, quantity, "repay" );
+}
+
+void minerSx::four( const string memo )
+{
+    require_auth( get_self() );
+
+    const name contract = "eosio.token"_n;
+    const asset balance = token::get_balance( contract, get_self(), symbol_code{"EOS"} );
+    check( balance.amount >= 1, "profit must exceed 0.0001 EOS");
+    transfer( get_self(), "fee.sx"_n, contract, balance, memo );
 }
--- miner.sx.hpp
+++ miner.sx.hpp
@@ -16,16 +16,34 @@ public:
     [[eosio::on_notify("flash.sx::callback")]]
     void on_callback( const name to, const name contract, asset quantity, const string memo, const name recipient );
 
+    [[eosio::action]]
+    void one();
+
+    [[eosio::action]]
+    void two();
+
+    [[eosio::action]]
+    void three( const name contract, const asset quantity );
+
+    [[eosio::action]]
+    void four( const string memo );
+
+    using mine_action = eosio::action_wrapper<"mine"_n, &minerSx::mine>;
+    using one_action = eosio::action_wrapper<"one"_n, &minerSx::one>;
+    using two_action = eosio::action_wrapper<"two"_n, &minerSx::two>;
+    using three_action = eosio::action_wrapper<"three"_n, &minerSx::three>;
+    using four_action = eosio::action_wrapper<"four"_n, &minerSx::four>;
+
     // static actions
     static void repay( const name contract, const asset quantity )
     {
         transfer( "miner.sx"_n, "flash.sx"_n, contract, quantity, "miner" );
     }
 
-    static void borrow( const name contract, const asset quantity )
+    static void borrow( const name contract, const asset quantity, const string executor )
     {
         flash::borrow_action borrow( "flash.sx"_n, { "miner.sx"_n, "active"_n });
-        borrow.send( "miner.sx"_n, contract, quantity, "miner", "miner.sx"_n );
+        borrow.send( "miner.sx"_n, contract, quantity, executor, "miner.sx"_n );
     }
 
     static void transfer( const name from, const name to, const name contract, const asset quantity, const string memo )
