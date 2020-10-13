eb8787c5f718b9f7e8a2ecc6f8d0739d517a0a90
Denis <carriere.denis@gmail.com>
2020-10-13 15:18:17 -0400

    add ms timing

--- src/utils.ts
+++ src/utils.ts
@@ -5,21 +5,26 @@ import { Action } from "eosjs/dist/eosjs-serialize";
  * Transaction
  */
 export async function transact(api: Api, actions: Action[]): Promise<string> {
+    const start = new Date().getTime();
     let trx_id: string;
     try {
         const result = await api.transact({actions}, { blocksBehind: 3, expireSeconds: 30 });
         trx_id = result.transaction_id;
+        const end = new Date().getTime();
+        const ms = (end - start) + "ms";
 
         for (const action of actions) {
-            console.log(`${action.account}::${action.name} [${JSON.stringify(action.data)}] => ${trx_id}`);
+            console.log(`${ms} ${action.account}::${action.name} [${JSON.stringify(action.data)}] => ${trx_id}`);
         }
     } catch (e) {
         if (e instanceof RpcError) {
+            const end = new Date().getTime();
             const {name, what, details} = e.json.error
             const message = (details[0]) ? details[0].message : `[${name}] ${what}`;
+            const ms = (end - start) + "ms";
 
             for (const action of actions) {
-                console.error(`ERROR ${action.account}::${action.name} [${JSON.stringify(action.data)}] => ${message}`);
+                console.error(`${ms} ERROR ${action.account}::${action.name} [${JSON.stringify(action.data)}] => ${message}`);
             }
         } else {
             console.error(e);
