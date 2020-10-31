9a3e84b5b9dd7be1f09bd4ce7d5887c8f8c35f4a
Denis <carriere.denis@gmail.com>
2020-10-30 21:54:41 -0400

    update eco system

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -1,10 +1,25 @@
 module.exports = {
     apps: [
         {
-            name: "sx.server",
+            name: "sx.server::sx",
             script: "index.ts",
             autorestart: true,
-            log_date_format : "YYYY-MM-DD HH:mm"
-        }
+            log_date_format : "YYYY-MM-DD HH:mm",
+            env: {
+                ACTOR="miner.sx",
+                TYPE: "sx"
+            }
+        },
+        {
+            name: "sx.server::gravy",
+            script: "index.ts",
+            autorestart: true,
+            log_date_format : "YYYY-MM-DD HH:mm",
+            env: {
+                ACTOR="gravy.sx",
+                CPU_ACTOR="miner.sx",
+                TYPE: "gravy"
+            }
+        },
     ]
 };
