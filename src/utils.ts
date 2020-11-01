ad8c3deea5db8943b4b6e368d4db567d4132befa
Denis <carriere.denis@gmail.com>
2020-10-31 22:37:41 -0400

    add minute counter

--- src/utils.ts
+++ src/utils.ts
@@ -1,12 +1,27 @@
 import { RpcError, Api } from "eosjs";
 import { Action } from "eosjs/dist/eosjs-serialize";
 
+let init = new Date().getTime();
+let count = 0;
+
 /**
  * Transaction
  */
 export async function transact(api: Api, actions: Action[]): Promise<string> {
+    // start time when transaction request
     const start = new Date().getTime();
     let trx_id: string;
+
+    // calculate aggregated transaction count per minute
+    count ++;
+    const count_m = Math.floor(count / ((start - init) / 1000 ) * 60);
+
+    // reset init timer if exceeds 1 hour
+    if ( start - init > 3600000 ) {
+        init = new Date().getTime();
+        count = 0;
+    }
+
     try {
         const result = await api.transact({actions}, { blocksBehind: 3, expireSeconds: 30 });
         trx_id = result.transaction_id;
@@ -14,7 +29,7 @@ export async function transact(api: Api, actions: Action[]): Promise<string> {
         const ms = (end - start) + "ms";
 
         for (const action of actions) {
-            console.log(`${ms} ${action.account}::${action.name} [${JSON.stringify(action.data)}] => ${trx_id}`);
+            console.log(`${ms} [${count_m}/m] ${action.account}::${action.name} [${JSON.stringify(action.data)}] => ${trx_id}`);
         }
     } catch (e) {
         if (e instanceof RpcError) {
@@ -24,7 +39,7 @@ export async function transact(api: Api, actions: Action[]): Promise<string> {
             const ms = (end - start) + "ms";
 
             for (const action of actions) {
-                console.error(`${ms} ERROR ${action.account}::${action.name} [${JSON.stringify(action.data)}] => ${message}`);
+                console.error(`${ms} [${count_m}/m] ERROR ${action.account}::${action.name} [${JSON.stringify(action.data)}] => ${message}`);
             }
         } else {
             console.error(e);
