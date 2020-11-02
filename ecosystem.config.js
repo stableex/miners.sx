0f3df5e8addc626539ffd6f84b36242f13cce5bc
Denis <carriere.denis@gmail.com>
2020-11-02 08:46:24 -0500

    update config

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -6,9 +6,24 @@ module.exports = {
             autorestart: true,
             log_date_format : "YYYY-MM-DD HH:mm",
             env: {
+                ACTOR: "miner.sx",
+                CPU_ACTOR: "miner.sx",
+                TYPE: "sx",
+                CONCURRENCY: 4,
+                TIMEOUT_MS: 20
+            }
+        },
+        {
+            name: "sx.server::gravy",
+            script: "index.ts",
+            autorestart: true,
+            log_date_format : "YYYY-MM-DD HH:mm",
+            env: {
                 ACTOR: "cpu.sx",
                 CPU_ACTOR: "cpu.sx",
-                TYPE: "sx"
+                TYPE: "gravy",
+                CONCURRENCY: 1,
+                TIMEOUT_MS: 20
             }
         },
         {
@@ -18,9 +33,10 @@ module.exports = {
             log_date_format : "YYYY-MM-DD HH:mm",
             env: {
                 ACTOR: "gravy.sx",
-                CPU_ACTOR: "miner.sx",
+                CPU_ACTOR: "cpu.sx",
                 TYPE: "gravy",
-                CONCURRENCY: 1
+                CONCURRENCY: 1,
+                TIMEOUT_MS: 100
             }
         },
         {
@@ -29,8 +45,8 @@ module.exports = {
             autorestart: true,
             log_date_format : "YYYY-MM-DD HH:mm",
             env: {
-                ACTOR: "miner.sx",
-                CPU_ACTOR: "miner.sx",
+                ACTOR: "cpu.sx",
+                CPU_ACTOR: "cpu.sx",
                 TYPE: "sapex",
                 CONCURRENCY: 1,
                 TIMEOUT_MS: 60000
